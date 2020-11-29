4aa6e712cd9dd2c736251f62b1bd1d96c02add53
agustinj <agustin.jauregui2@gmail.com>
2020-11-29 19:47:11 +0100

    Added @CacheLookup annotations in ContactsPage class

--- FreeCRMTestAutomation/src/main/java/com/crm/qa/pages/ContactsPage.java
+++ FreeCRMTestAutomation/src/main/java/com/crm/qa/pages/ContactsPage.java
@@ -2,6 +2,7 @@ package com.crm.qa.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
@@ -11,21 +12,26 @@ import com.crm.qa.base.TestBase;
 public class ContactsPage extends TestBase {
 	
 	@FindBy(xpath = "//td[contains(text(),'Contacts')]")
+	@CacheLookup
 	WebElement contactsPageLabel;
 	
 //	@FindBy(name = "title")
 //	WebElement titleDropdown;
 	
 	@FindBy(id = "first_name")
+	@CacheLookup
 	WebElement firstName;
 	
 	@FindBy(id = "surname")
+	@CacheLookup
 	WebElement lastName;
 	
 	@FindBy(name = "client_lookup")
+	@CacheLookup
 	WebElement company;
 	
 	@FindBy(xpath = "//body[1]/table[2]/tbody[1]/tr[1]/td[2]/table[1]/tbody[1]/tr[1]/td[1]/fieldset[1]/form[1]/table[1]/tbody[1]/tr[1]/td[1]/input[2]")
+	@CacheLookup
 	WebElement saveBtn;
 	
 	public ContactsPage() {
